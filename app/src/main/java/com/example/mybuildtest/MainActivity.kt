bdf87f81076eccccfc730aa5447e84a76c9af323
siuhangw <wshiahb@gmail.com>
2025-06-27 12:30:12 +0800

    Content update for mainactivity.

--- app/src/main/java/com/example/mybuildtest/MainActivity.kt
+++ app/src/main/java/com/example/mybuildtest/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyBuildTestTheme {
-        Greeting("Android this is a new release v0.0.2!")
+        Greeting("Android this is a new release v0.0.3!")
     }
 }
